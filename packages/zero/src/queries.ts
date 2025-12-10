32853c11744d32209816aca998766bf32aceb103
Chase Adams <c@cadams.io>
2025-12-09 17:32:50 -0700

    chore: align with demo vid

--- packages/zero/src/queries.ts
+++ packages/zero/src/queries.ts
@@ -1,17 +1,13 @@
-import {defineQuery} from '@rocicorp/zero';
+import {defineQueries, defineQuery} from '@rocicorp/zero';
 import {z} from 'zod';
 import {zql} from './schema';
 
-export const queries = {
-  albumsByArtist: defineQuery(
-    'albumsByArtist',
-    {
-      validator: z.object({artistID: z.string()}),
-    },
-    ({args: {artistID}}) =>
-      zql.albums
-        .where('artistId', artistID)
-        .orderBy('createdAt', 'asc')
-        .limit(10),
+export const queries = defineQueries({
+  albumsByArtist: defineQuery(z.object({artistID: z.string()}), ({args}) =>
+    zql.albums
+      .where('artistId', args.artistID)
+      .orderBy('createdAt', 'asc')
+      .limit(10)
+      .related('artist', q => q.one()),
   ),
-};
+});
