8ed684eb20740deebab011c6d74395b9967c48d8
Chase Adams <gh@cadams.io>
2025-12-10 15:02:49 -0700

    feat(zero): added zero!

--- apps/web/src/routes/api/mutate.ts
+++ apps/web/src/routes/api/mutate.ts
@@ -1,1 +1,26 @@
-// TODO: Implement mutate route
+import {mustGetMutator} from '@rocicorp/zero';
+import {handleMutateRequest} from '@rocicorp/zero/server';
+import {createFileRoute} from '@tanstack/react-router';
+import {json} from '@tanstack/react-start';
+import {mutators} from '@zero-onboarding/zero';
+import {dbProvider} from '../../db-provider.ts';
+
+export const Route = createFileRoute('/api/mutate')({
+  server: {
+    handlers: {
+      POST: async ({request}) => {
+        const result = await handleMutateRequest(
+          dbProvider,
+          transact =>
+            transact((tx, name, args) => {
+              const mutator = mustGetMutator(mutators, name);
+              return mutator.fn({args, tx, ctx: {userId: 'anon'}});
+            }),
+          request,
+        );
+
+        return json(result);
+      },
+    },
+  },
+});
