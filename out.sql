05e5cc2a99227bf2b6a4fd264f41460b580b24fc
Chase Adams <gh@cadams.io>
2026-04-30 15:37:40 -0700

    chore: fix up out.sql

--- out.sql
+++ out.sql
@@ -1,14 +1,14 @@
 -- Drop favorites first
-DROP TABLE IF EXISTS "_z_favorites";--> statement-breakpoint
+DROP TABLE IF EXISTS "public"."_z_favorites" CASCADE;--> statement-breakpoint
 
 -- Drop albums
-DROP TABLE IF EXISTS "_z_albums";--> statement-breakpoint
+DROP TABLE IF EXISTS "public"."_z_albums" CASCADE;--> statement-breakpoint
 
 -- Drop artists
-DROP TABLE IF EXISTS "_z_artists";--> statement-breakpoint
+DROP TABLE IF EXISTS "public"."_z_artists" CASCADE;--> statement-breakpoint
 
 -- Drop fans last
-DROP TABLE IF EXISTS "_z_fans";--> statement-breakpoint
+DROP TABLE IF EXISTS "public"."_z_fans" CASCADE;--> statement-breakpoint
 
 -- Print a log message :)
 DO $$
